eb64d85fd76dc0267bffbecf22cf6d12f951d6c2
Rob Eyre <rob.eyre@ricardo-aea.com>
2015-03-21 14:44:33 +0000

    Update to add timestamp

--- bin/latest-by-site.php
+++ bin/latest-by-site.php
@@ -3,10 +3,11 @@
 $xml = \file_get_contents('http://uk-air.defra.gov.uk/assets/rss/current_site_levels.xml');
 $doc = \simplexml_load_string($xml);
 $fh = \fopen(__DIR__ . '/current_site_levels.csv', 'wb');
-\fputcsv($fh, array('Site', 'Latitude', 'Longitude', 'Index'));
+\fputcsv($fh, array('Site', 'Latitude', 'Longitude', 'Index', 'Time'));
 foreach ($doc->channel->item as $item) {
     $title = \trim($item->title);
     $description = \html_entity_decode((string) $item->description, ENT_COMPAT, 'UTF-8');
+    $time = new \DateTime((string) $item->pubDate);
     $match = null;
     if (\preg_match('/Location: ([-0-9]+)°([0-9]+)´([.0-9]+)"N.*([-0-9]+)°([0-9]+)´([.0-9]+)"W.*at index ([0-9]+)/', $description, $match)) {
         $lat = $match[1] + $match[2]/60 + $match[3]/3600;
@@ -17,6 +18,7 @@ foreach ($doc->channel->item as $item) {
             'latitude' => $lat,
             'longitude' => $long,
             'index' => $index,
+            'time' => $time->format('Y-m-d H:i:s'),
         ));
     }
 }
